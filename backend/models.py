aa5a30990e82d9e51f6224eb24cc9e6999a84d17
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2024-09-08 20:29:18 +0200

    Post Model

--- backend/models.py
+++ backend/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class User(models.Models):
-    user_id = models.AutoField(primary=True)
+    user_id = models.AutoField(primary=True, auto_now_add=True)
     first_name = models.CharField(max_legnth=50)
     last_name = models.CharField(max_legnth=50)
     email = models.EmailField(max_legnth=100, unique=True)
@@ -12,6 +12,16 @@ class User(models.Models):
     profile_image = models.URLField(max_legnth=250)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
+class Post(models.MOdels):
+    post_id = models.CharField(primary=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    post_text = models.CharField(max_legnth=500)
+    post_image = models.URLField(max_legnth=250)
+    likes = models.IntegerField()
+
+
 
 
 
