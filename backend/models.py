14e9adb91193f143b141557d75860a59392297b3
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2024-09-08 20:04:04 +0200

    user model

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Models):
+    user_id = models.AutoField(primary=True)
+    first_name = models.CharField(max_legnth=50)
+    last_name = models.CharField(max_legnth=50)
+    email = models.EmailField(max_legnth=100, unique=True)
+    password = models.CharField(max_legnth=20)
+    profile_image = models.URLField(max_legnth=250)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+
+
+
+
 
 
