fab0a9dfbd797670aad810d59f27a107424729bc
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2024-09-08 20:45:55 +0200

    model tweaks

--- backend/models.py
+++ backend/models.py
@@ -3,30 +3,30 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Models):
-    user_id = models.AutoField(primary=True, auto_now_add=True)
-    first_name = models.CharField(max_legnth=50)
-    last_name = models.CharField(max_legnth=50)
-    email = models.EmailField(max_legnth=100, unique=True)
-    password = models.CharField(max_legnth=20)
-    profile_image = models.URLField(max_legnth=250)
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=20)
+    profile_image = models.URLField(max_length=250)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-class Post(models.MOdels):
-    post_id = models.CharField(primary=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
-    post_text = models.CharField(max_legnth=500)
-    post_image = models.URLField(max_legnth=250)
+    post_text = models.CharField(max_length=500)
+    post_image = models.URLField(max_length=250)
     likes = models.IntegerField()
 
 
-class Comments(models.Model):
-    comment_id = models.IntegerField(primary=True)
-    post_id = models.ForeignKey(Post)
-    user_id = models.ForeignKey(User)
-    text = models.CharField(max_legnth=500)
+class Comment(models.Model):
+    comment_id = models.AutoField(primary_key=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
 
 
 
