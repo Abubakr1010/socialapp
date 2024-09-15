6866417a56331c6304950be15cd8d516ec2ad926
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2024-09-15 16:38:12 +0200

    user_id view return in sign_up

--- backend/tests/test_signup.py
+++ backend/tests/test_signup.py
@@ -2,6 +2,7 @@ import pytest
 from rest_framework.test import APIClient
 from rest_framework import status
 from django.urls import reverse
+from backend.models import User
 
 
 
@@ -25,6 +26,18 @@ def test_if_user_signedin_return_201():
     response = client.post(url, data, format = 'json')
 
     assert response.status_code == status.HTTP_201_CREATED
+
+    user = User.objects.get(email=email)
+
+    expected_data = {
+        "user_id":user.user_id,
+        "first_name":user.first_name,
+        "last_name":user.last_name,
+        "email":user.email,
+    }
+
+    response.data.pop('password', None)
+    assert response.data == expected_data
 
 
 
--- backend/views.py
+++ backend/views.py
@@ -3,7 +3,6 @@ from .serializer import Signup
 from rest_framework.decorators import action
 from rest_framework import status, viewsets
 from rest_framework.response import Response
-# Create your views here.
 
 
 #signup view
@@ -12,8 +11,9 @@ class SignupViewSet(viewsets.ViewSet):
     def signup(self,request):
             serializer = Signup(data=request.data)
             if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status= status.HTTP_201_CREATED)
+                user = serializer.save()
+                response_data= {"user_id": user.user_id, **serializer.data}
+                return Response(response_data, status= status.HTTP_201_CREATED)
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
