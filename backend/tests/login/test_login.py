86075697e434d180ec6c73cac198dc65ce6281e2
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2024-09-18 17:29:59 +0200

    login successfull test pytest completed

---
+++ backend/tests/login/test_login.py
@@ -0,0 +1,35 @@
+import pytest
+from backend.models import User
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+
+
+class LoginViewSetTest(APITestCase):
+
+    def setUp(self):
+        # This method runs before each test
+        self.create_user()
+        self.login_url = '/login/login/'
+
+    def create_user(self):
+        self.user = User.objects.create_user(
+                               first_name='jack',
+                               last_name='max',
+                               email = 'jackmax@gmail.com',
+                               password = 'password123'
+        )
+
+    def test_login_successfull_return_200(self):
+        data={
+            'email': 'jackmax@gmail.com',
+            'password':'password123'
+        }
+
+        response = self.client.post(self.login_url,data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+
+
