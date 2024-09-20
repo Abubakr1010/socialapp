06c829040b68854613c981fa8f4eef2e2825547a
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2024-09-20 17:32:47 +0200

    wrong password pytest completed

--- backend/tests/login/test_login.py
+++ backend/tests/login/test_login.py
@@ -31,6 +31,13 @@ class LoginViewSetTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_login_wrong_password_return_400(self):
+        data ={
+            'email':'jackmax@gmail.com',
+            'password':'password122'
+        }
+
+        response = self.client.post(self.login_url,data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
 
