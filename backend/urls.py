53350e8f4ee3aedfc4970738ba1d862d28e6cbf6
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2024-09-26 14:23:17 +0200

    Home view set successfully created

--- backend/urls.py
+++ backend/urls.py
@@ -8,6 +8,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,
 urlpatterns = [
     path('signup/', views.SignupViewSet.as_view({'post': 'signup'}, name='signup')),
     path('login/', views.LoginViewSet.as_view({'post': 'login'}), name='login'),
+    path('home/<int:pk>/', views.HomeViewSet.as_view({'get':'home'}), name='home'),
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
