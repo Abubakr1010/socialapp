e33d05017888b478014132f895226552fc41da2b
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2024-09-26 14:14:13 +0200

    Login and Signup routers updated to manually rigistered

--- backend/urls.py
+++ backend/urls.py
@@ -5,19 +5,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,
 
 
 
-
-router = routers.DefaultRouter()
-router.register('signup', views.SignupViewSet, basename='signup')
-router.register('login', views.LoginViewSet, basename='login')
-
-login_router = routers.NestedDefaultRouter(router, 'login', lookup='login')
-login_router.register('home', views.HomeViewSet, basename='home')
-
-login_router.register('home/createpost', views.CreatePostViewSet, 'createpost')
-
 urlpatterns = [
-    path('', include(router.urls)),
-    path('', include(login_router.urls)),
+    path('signup/', views.SignupViewSet.as_view({'post': 'signup'}, name='signup')),
+    path('login/', views.LoginViewSet.as_view({'post': 'login'}), name='login'),
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
